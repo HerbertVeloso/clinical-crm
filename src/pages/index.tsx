41a62a8b375cff9c43960eaee1e5f67d4eb4acd8
Herbert Veloso <herbert.oliveira.veloso@gmail.com>
2022-11-11 22:50:18 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Clinical CRM</title>
+        <meta
+          name="description"
+          content="Faça a gestão dos pacientes da sua clinica"
+        />
+      </Head>
+
+      <h1>Clinical CRM</h1>
+    </>
+  );
+}
